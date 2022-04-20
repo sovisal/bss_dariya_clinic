bb646ea183de767e56ba56887b9fd62ce24322b5
SokunSovisal <sovisalsokun@gmail.com>
2022-04-20 22:09:17 +0700

    Doctor CRUD

--- database/migrations/2022_03_31_231104_create_doctors_table.php
+++ database/migrations/2022_03_31_231104_create_doctors_table.php
@@ -6,26 +6,47 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateDoctorsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('doctors', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('doctors', function (Blueprint $table) {
+			$table->id();
+			$table->string('name_kh');
+			$table->string('name_en')->nullable();
+			$table->string('id_card_no')->nullable();
+			$table->boolean('gender')->nullable();
+			$table->string('phone')->nullable();
+			$table->string('email')->nullable();
+			$table->text('address')->nullable();
+			$table->unsignedBigInteger('created_by');
+			$table->unsignedBigInteger('updated_by');
+			$table->timestamps();
+			
+			$table->foreign('created_by')
+					->references('id')
+					->on('users')
+					->onUpdate('cascade')
+					->onDelete('cascade');
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('doctors');
-    }
+			$table->foreign('updated_by')
+					->references('id')
+					->on('users')
+					->onUpdate('cascade')
+					->onDelete('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('doctors');
+	}
 }
--- resources/views/doctor/index.blade.php
+++ resources/views/doctor/index.blade.php
@@ -1,21 +1,48 @@
 <x-app-layout>
-	<x-card :foot="false"  :head="false">
-		<x-form.button href="#" class="" color="primary" label="Create" />			
-		<x-table class="table-hover table-bordered" id="datatables" data-table="patients">
+	<x-slot name="header">
+		<x-form.button href="{{ route('setting.doctor.create') }}" icon="bx bx-plus" label="Create" />	
+	</x-slot>
+	<x-card :foot="false"  :head="false">		
+		<x-table class="table-hover table-bordered" id="datatables">
 			<x-slot name="thead">
 				<tr>
-					<th>No</th>
-					<th>Doctor Name</th>
-					<th>Description</th>
+					<th>Code</th>
+					<th>Name Kh</th>
+					<th>Name En</th>
+					<th>Gender</th>
+					<th>Phone</th>
+					<th>Modify at</th>
+					<th>Modify by</th>
+					<th>Action</th>
 				</tr>
 			</x-slot>
-			@foreach([0,1,2,3,4,5,6] as $addr)
+			@foreach($doctors as $doctor)
 				<tr>
-					<td class="text-center">111</td>
-					<td>111</td>
-					<td>222</td>
+					<td class="text-center">
+						DT-{!! str_pad($doctor->id, 6, '0', STR_PAD_LEFT) !!}
+					</td>
+					<td>{{ $doctor->name_kh }}</td>
+					<td>{{ $doctor->name_en }}</td>
+					<td>{{ (($doctor->gender)? 'Female' : 'Male') }}</td>
+					<td>{{ $doctor->phone }}</td>
+					<td>{!! date('d-M-Y H:i', strtotime($doctor->updated_at)) !!}</td>
+					<td>{!! $doctor->updated_by_name !!}</td>
+					<td class="text-center">
+						@can('UpdateDoctor')
+							<x-form.button color="secondary" class="btn-sm" href="{{ route('setting.doctor.edit', $doctor->id) }}" icon="bx bx-edit-alt" />
+						@endcan
+						@can('DeleteDoctor')
+							<x-form.button color="danger" class="confirmDelete btn-sm" data-id="{{ $doctor->id }}" icon="bx bx-trash" />
+							<form class="sr-only" id="form-delete-{{ $doctor->id }}" action="{{ route('setting.doctor.delete', $doctor->id) }}" method="POST">
+								@csrf
+								@method('DELETE')
+								<button class="sr-only" id="btn-{{ $doctor->id }}">Delete</button>
+							</form>
+						@endcan
+					</td>
 				</tr>
 			@endforeach
 		</x-table>
 	</x-card>
+	<x-modal-confirm-delete />
 </x-app-layout>
